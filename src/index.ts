ab4485d11f4c47b53e75868e6487a47b5623d786
user52dev <user52dev@gmail.com>
2024-09-02 17:34:36 +0300

    user profile features

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,29 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/myUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/myRestaurantRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to database!"));
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "health ok!" });
 });
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(7000, () => {
   console.log("Server started on localhost:7000");
